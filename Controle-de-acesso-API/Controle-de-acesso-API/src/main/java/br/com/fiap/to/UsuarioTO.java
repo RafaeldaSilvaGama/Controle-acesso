86622a543ac33b41992b5a2c8b16824e2f750926
RSGama <101108076+RafaeldaSilvaGama@users.noreply.github.com>
2022-10-26 08:11:37 -0300

    Add files via upload

---
+++ Controle-de-acesso-API/Controle-de-acesso-API/src/main/java/br/com/fiap/to/UsuarioTO.java
@@ -0,0 +1,39 @@
+package br.com.fiap.to;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
+public class UsuarioTO {
+	private String login;
+	private String senha;
+	
+	
+
+	public UsuarioTO() {
+
+	}
+
+	public UsuarioTO(String login, String senha) {
+		super();
+		this.login = login;
+		this.senha = senha;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
